1f693d3201dcde74e20c94e083590e5e703fd0d6
Jimmy Grados <died_sistemas25@minedu.gob.pe>
2024-09-16 21:20:46 -0500

    Add project files.

---
+++ CsvDemo/CustomerModel.cs
@@ -0,0 +1,43 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace CsvDemo
+{
+    public class CustomerModel
+    {        
+        [Name("Index")]
+        public int Index { get; set; }
+
+        [Name("Customer Id")]
+        public string CustomerId { get; set; }
+
+        [Name("First Name")]
+        public string FirstName { get; set; }
+
+        [Name("Last Name")]
+        public string LastName { get; set; }
+
+        [Name("Company")]
+        public string Company { get; set; }
+
+        [Name("City")]
+        public string City { get; set; }
+
+        [Name("Country")]
+        public string Country { get; set; }
+
+        [Name("Phone 1")]
+        public string Phone1 { get; set; }
+
+        [Name("Phone 2")]
+        public string Phone2 { get; set; }
+
+        [Name("Email")]
+        public string Email { get; set; }
+
+        [Name("Subscription Date")]
+        public DateTime SubscriptionDate { get; set; }
+
+        [Name("Website")]
+        public string Website { get; set; }
+    }
+}
---
+++ CsvDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿// Comma Separated Values (CSV) Demo
+// 1,Tim,Corey,555-1212
+
+using CsvDemo;
+using CsvHelper;
+using System.Globalization;
+
+//string inputFile = @"~\files\customers-1000.csv";
+//string outputFile = @"~\files\filtered-customers.csv";
+
+string inputFile = @"C:\Users\jimmy\source\repos\IntroToCsvHelper\CsvDemo\files\customers-100.csv";
+string outputFile = @"C:\Users\jimmy\source\repos\IntroToCsvHelper\CsvDemo\files\filtered-customers.csv";
+
+List<CustomerModel> outputRecords = new();
+
+using var reader = new StreamReader(inputFile);
+using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+int i = 0;
+
+var records = csv.GetRecords<CustomerModel>();
+
+foreach (var record in records)
+{
+    if (record.FirstName.StartsWith("S"))
+    {
+        outputRecords.Add(record);
+    }
+    Console.WriteLine(record.FirstName + " " + record.LastName);
+    i++;
+}
+
+Console.WriteLine($"Number of records: {outputRecords.Count}");
+
+using var writer = new StreamWriter(outputFile);
+using var csvOut = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+csvOut.WriteRecords(outputRecords);
+
+Console.ReadLine();
